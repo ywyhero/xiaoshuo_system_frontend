1f38a125011142b38bad3851b87b3531c935f634
yaoweiyu <yaoweiyu@peilian.com>
2019-11-08 18:06:11 +0800

    切换域名

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,14 +3,14 @@ const CONFIG = {
         api: 'http://192.168.44.132:3000/api',
     },
     PROD: {
-        api: 'http://47.105.109.192:3000/api',
+        api: 'http://www.vinekan.com:3000/api',
     },
 };
 interface ConfigObj {
     api: string;
 }
 let  ENV: ConfigObj;
-if (window.location.href.includes('47.105.109.192')) {
+if (window.location.href.includes('www.vinekan.com')) {
     ENV = CONFIG.PROD;
 } else {
     ENV = CONFIG.LOCAL;
