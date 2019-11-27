9f30420f01418bcce3354af08cd3616b3d8c35c7
yaoweiyu <yaoweiyu@peilian.com>
2019-11-27 15:47:24 +0800

    test

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,15 +3,15 @@ const CONFIG = {
         api: 'http://192.168.44.96:3000/api',
     },
     PROD: {
-        api: 'https://www.vinekan.com:3000/api',
-        // api: 'http://47.105.109.192:3000/api',
+        // api: 'https://www.vinekan.com:3000/api',
+        api: 'http://47.105.109.192:3000/api',
     },
 };
 interface ConfigObj {
     api: string;
 }
 let  ENV: ConfigObj;
-if (window.location.href.includes('www.vinekan.com')) {
+if (window.location.href.includes('47.105.109.192')) {
     ENV = CONFIG.PROD;
 } else {
     ENV = CONFIG.LOCAL;
