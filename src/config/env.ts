dcc11a687a4f22d691b10b8d6ce9b909c686d094
yaoweiyu <yaoweiyu@peilian.com>
2019-12-04 23:21:46 +0800

    添加爬虫

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ const CONFIG = {
         api: 'http://192.168.44.96:3000/api',
     },
     PROD: {
-        api: 'https://www.vinekan.com:3000/api',
+        api: 'http://www.vinekan.com:3000/api',
         // api: 'http://47.105.109.192:3000/api',
     },
 };
