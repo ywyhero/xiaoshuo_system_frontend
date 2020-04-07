7b0baa8e8e6cbbeb18c489ff0136783ea6ef3576
yaoweiyu <yaoweiyu@peilian.com>
2020-04-07 17:29:16 +0800

    优化

--- src/views/addChapters.vue
+++ src/views/addChapters.vue
@@ -21,8 +21,12 @@
             <el-input v-model="htmlCharset" placeholder="编码"/>
         </div>
         <div class="add-chapters-li">
-            <span class="add-chapters-li-val">章节链接是否有主域名：</span>
+            <span class="add-chapters-li-val">开始上传的章节Id：</span>
             <el-input v-model.number="initCount" placeholder="请填写初始值，默认0"/>
+        </div>
+        <div class="add-chapters-li">
+            <span class="add-chapters-li-val">需要额外添加的章节数：</span>
+            <el-input v-model.number="otherCount" placeholder="请填写初始值，默认0"/>
         </div>
         <div class="add-chapters-li">
             <span class="add-chapters-li-val">域名：</span>
@@ -47,6 +51,7 @@ export default class AddChapters extends Vue {
     private url: string = '';
     private host: string = '';
     private initCount: number = 0;
+    private otherCount: number = 0;
     private htmlCharset: string = '';
     private chapterClassId: string = '';
     private contentClassId: string = '';
@@ -62,6 +67,7 @@ export default class AddChapters extends Vue {
             contentClassId: this.contentClassId,
             host: this.host,
             initCount: this.initCount,
+            otherCount: this.otherCount,
             htmlCharset: this.htmlCharset,
             url: this.url,
             bookId: this.bookId,
